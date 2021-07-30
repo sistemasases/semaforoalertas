4a8221984c09fa92f728e4153e7f86755608dcb1
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2021-07-30 16:14:09 -0500

    Add files via upload

--- js/versionMovil.js
+++ js/versionMovil.js
@@ -101,20 +101,32 @@ function screenResponsiveWebDesing() {
         $('.contact-information').text('Mayor información')
 
         $('.tooltip-movil-inicio').fadeIn()
-
-        $('#hamburger-movil').css({
-            'animation': 'colorHamburger 3.5s 1'
-        })
-        setTimeout(function () {
-        $('#hamburger-movil').addClass('filter-aqua')
-        },700)
-
         $('.tooltip-movil-inicio').text(textTooltipDimension)
         $('.tooltip-movil-inicio').delay(3000).fadeOut()
+
+        //animacion el hamburger de las dimensiones 
+        $('.hamburger-btn-movil').css({
+            'animation': 'colorHamburger 0.5s 1',
+            'color': 'rgb(255,255,255,100)'
+        })
+
         setTimeout(function () {
-        $('#hamburger-movil').removeClass('filter-aqua')
-        $('#hamburger-movil').attr({style: ''})
-        }, 3100)
+        $('#hamburger-movil').addClass('filter-aqua')
+        }, 250)
+
+        //quitar animacion con el tiempo
+        setTimeout(function () {
+
+        $('.hamburger-btn-movil').css({
+            'animation': 'colorGray 2.5s 1',
+            'color': 'rgb(128,128,128,100)'
+        })
+
+        }, 2900)
+
+        setTimeout(function () {
+        $('#hamburger-movil').removeClass('filter-aqua')    
+        }, 3000)
     
 
         $('.menu-item').each(function (index,value) {
@@ -251,10 +263,11 @@ function dimensionFuncionalityMovil(dimension , dimensionPrecionada) {
         })
 
         //apagar animaciones anteriores
+        $('.tooltip-movil').fadeOut().stop()
         $('.tooltip-movil-inicio').fadeIn().stop()
 
         //animaciones en los tooltip
-        $('#hamburger-movil').attr({style: ''})
+        clearTimeout()           
         $('.tooltip-movil-inicio').fadeIn()
         $('#hamburger-tematica').addClass('filter-white')
         $('.tooltip-movil-inicio').text(" Haz clic en las Tematicas para inicar")
@@ -487,8 +500,8 @@ function dimensionFuncionalityMovil(dimension , dimensionPrecionada) {
                   
                  }else {
                   $('.title-movil').css({
-                      'font-size': '0.7em',
-                      'right': '-1em'
+                      'font-size': '0.65em',
+                      'right': '-0.5em'
                   })    
                  }
 
@@ -521,12 +534,27 @@ function tematicasFuncionalityMovil(NombreDimension , tematicaPresionada, indexT
         }
         setTimeout(function () {
         $('#hamburger-tematica').removeClass('filter-white')
-        }, 3000)
+        }, 3500)
 
         quitRiskMovil()
 
         semaforoMovil(tematicaPresionada)
         restarAnimateFooterMovil()
+
+        $('.tematica-nav').each(function(index,value) {
+
+         if ( ($(value).find('.tematica-nav-btn')).hasClass('active') ) {
+          tematicaDimension = ( ( $(this).parent() ).parent() ).parent()
+          navsInDimension = tematicaDimension.find('.tematica-nav')
+          indexTematica = navsInDimension.index(this)
+          $(value).attr({'id' : 'activo'})
+         }
+         else {
+         $(value).attr({'id' : ''})
+         }
+
+        })
+
         switch (NombreDimension) {
                
             case '#individual':
@@ -751,17 +779,29 @@ function inicioMovil() {
         $('#semaforo-item-'+i+'').show()
         }
         
-        $('#hamburger-movil').css({
-            'animation': 'colorHamburger 3.5s 1'
+        //animacion del hamburger
+        $('.hamburger-btn-movil').css({
+            'animation': 'colorHamburger 0.5s 1',
+            'color': 'rgb(255,255,255,100)'
         })
-        setTimeout(function () {
-        $('#hamburger-movil').addClass('filter-aqua')
-        },700)
 
         setTimeout(function () {
-        $('#hamburger-movil').removeClass('filter-aqua')
-        $('#hamburger-movil').attr({style: ''})
-        }, 3100)
+        $('#hamburger-movil').addClass('filter-aqua')
+        }, 250)
+
+        //quitar animacion del hamburger
+        setTimeout(function () {
+
+        $('.hamburger-btn-movil').css({
+            'animation': 'colorGray 2.5s 1',
+            'color': 'rgb(128,128,128,100)'
+        })
+
+        }, 2900)
+
+        setTimeout(function () {
+        $('#hamburger-movil').removeClass('filter-aqua')    
+        }, 3000)
 
         if ( sizeScreenWidth  <= 500 &&  sizeScreenWidth  > 375) {
         $('.semaforo').css({'left': '-4%'})
@@ -862,6 +902,9 @@ function quitRiskMovil() {
         'padding-bottom': '50%'
     })
     
+    $('.semaforo').find('.circle').each(function(e) {
+    $(this).removeClass('filter')
+    })
 
     if ( $('#inicio').hasClass('active')  ) {
 
